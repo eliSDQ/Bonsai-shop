b28a21b167d770c57a588e54cf4efa20ce19bc7d
EliSDQ <141088531+eliSDQ@users.noreply.github.com>
2025-11-04 17:41:34 +0330

    start styling body(sec2 plans)

---
+++ main/script.js
@@ -0,0 +1,8 @@
+let packsObj={
+    starter:{title:"Starter",price:24,desc:"Ideal for freelancers and contractors just starting out.",
+        features:["Ideal for freelancers and contractors just starting out.","Unlimited Clients & Projects","Invoicing & Payments","Proposals & Contracts","Tasks & Time Tracking","Client CRM","Expense Tracking","Up to 5 Project Collaborators"]},
+    Professional:{title:"Professional",price:39,desc:"Everything a growing independent business needs to thrive.",features:["Ideal for freelancers and contractors just starting out.","Unlimited Clients & Projects","Invoicing & Payments","Proposals & Contracts","Tasks & Time Tracking","Client CRM","Expense Tracking","Up to 5 Project Collaborators","Proposals & Contracts"]},
+    Business:{title:"Business",price:79,desc:"The perfect package for small businesses and agencies.",features:["Ideal for freelancers and contractors just starting out.","Unlimited Clients & Projects","Invoicing & Payments","Proposals & Contracts","Tasks & Time Tracking","Client CRM","Expense Tracking","Up to 5 Project Collaborators","Proposals & Contracts","Proposals & Contracts","3 Team Seats (additional seats $9/month)"]}
+}
+
+let $= document
