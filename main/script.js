78b2157b2bf80355f10a062b8611276143580977
EliSDQ <141088531+eliSDQ@users.noreply.github.com>
2025-11-06 15:11:42 +0330

    styling article+js

--- main/script.js
+++ main/script.js
@@ -1,6 +1,7 @@
 let $= document
 let sec2= $.getElementById("body-sec2")
 let sec3=$.getElementById("body-sec3")
+let article=$.getElementById("article-ul")
 let packsObj={
     starter:{title:"Starter",price:24,desc:"Ideal for freelancers and contractors just starting out.",
         features:["Ideal for freelancers and contractors just starting out.","Unlimited Clients & Projects","Invoicing & Payments","Proposals & Contracts","Tasks & Time Tracking","Client CRM","Expense Tracking","Up to 5 Project Collaborators"],popular:null},
@@ -47,7 +48,6 @@ Object.values(packsObj).forEach(elem => {
                 </ul>
                 <button class="bg-emerald-500 uppercase font-semibold rounded-md w-full h-15 my-10 text-white cursor-pointer">start free</button>
               `
-              console.log(newPack)
               sec2.insertAdjacentHTML("beforeend",newPack)
 });
 
@@ -69,4 +69,30 @@ Object.values(plans).forEach(elem=>{
   `
           
           sec3.insertAdjacentHTML("beforeend",newPlan)
+})
+
+let descArr=[
+  {title:"How does the free trial work?", desc:"When you start your trial with Bonsai you'll receive full, unlimited access to all of Bonsai's Workflow or Workflow Plus Features! You will need to enter your credit card information to begin your trial, but don't worry - we won't charge you anything until the subscription ends! If you wish to cancel at any time during the trial period, you can do so through your Subscriptions Settings Page."},
+    {title:"Can I change plans anytime?", desc:"Yes, you can from within your account. If you have already subscribed to a plan, or want to downgrade or upgrade your current one, you can do this by going to your \"Settings\" and \"Subscription\"."},
+    {title:"How do I pause my Bonsai subscription?", desc:"We totally understand that with the nature of freelancing, work ebbs and flows so you might not always need your Bonsai subscription to remain active! The good news is that you can cancel or pause your monthly subscription at any time without penalty. Once cancelled, you'll be able to continue logging in to access all your documents and even continue to use our free features, like Time Tracking! In order to cancel your subscription, login to your Bonsai account."},
+    {title:"What is your refund policy?", desc:"If you contact us within 2 weeks of being charged for your subscription, we will be happy to issue a refund for you!Beyond those 2 weeks, you will need to cancel or modify the subscription from the Subscriptions Tab in Settings to avoid future charges, but refunds will not be issued. This applies to both monthly and annual plans."}
+]
+
+descArr.forEach(i=>{
+  let newLi= $.createElement("li")
+  newLi.innerHTML=i.title
+  // newLi.className="cursor-pointer hover:text-emerald-500 w-fit select-none"
+  
+  newLi.addEventListener("click",()=>{
+   if(newLi.classList.contains("des")){
+    newLi.classList.remove("des")
+    newLi.lastChild.remove()
+   }else{
+    newLi.classList.add("des")
+    let newP= $.createElement("p")
+    newP.innerHTML=i.desc
+    newLi.append(newP)
+   }
+  })
+  article.append(newLi)
 })
